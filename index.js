4bf244e790a755f2d90ffa84688f1923cd47102c
Francesco <francesco.moretti2803@gmail.com>
2026-04-23 12:20:45 +0200

    funzione: cloudinary per conservazione dlele immagini e endpoint per test di caricamento

--- index.js
+++ index.js
@@ -7,13 +7,48 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));// Serve i file statici dalla cartella public
 
-// Serve i file statici dalla cartella public
-app.use(express.static('public'));
+const pool = require('./src/db');
+const { upload, cloudinary } = require('./src/cloudinaryConfig');
 
-app.get('/api/test', (req, res) => {
-    res.json({ message: "Il server è vivo e vegeto!" });
+//endpoint per test di upload immagini su cloudinary
+app.post('/api/edizioni/test-upload', upload.single('immagine'), (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: "Nessun file selezionato." });
+        }
+        // Determiniamo la cartella
+        let subfolder = "";
+        if(req.originalUrl.includes('edizioni')){
+            subfolder="edizioni";
+        }else{
+            subfolder="stampe";
+        }
+        // Caricamento manuale tramite Stream
+        const uploadStream = cloudinary.uploader.upload_stream(
+            { folder: `archivio_musicale/${subfolder}` },
+            (error, result) => {
+                if (error) {
+                    console.error("Errore Cloudinary:", error);
+                    return res.status(500).json({ error: "Errore durante l'upload su Cloudinary." });
+                }
+                // Se tutto ok, risposta con url dell'immagine
+                res.json({
+                    message: "Caricamento riuscito!",
+                    url: result.secure_url,
+                    public_id: result.public_id
+                });
+            }
+        );
+        // Scrivo i byte del file nello stream di Cloudinary
+        uploadStream.end(req.file.buffer);
+    } catch (err) {
+        console.error("Errore Server:", err);
+        res.status(500).json({ error: "Errore interno del server." });
+    }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server in esecuzione sulla porta ${PORT}`);
