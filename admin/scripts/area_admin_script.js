eef0235b2b90e4a0c488afc9b24b6c64f6bb4905
Francesco <francesco.moretti2803@gmail.com>
2026-05-06 13:03:21 +0200

    modifica stampe, snelliti id messaggi (=>querySelector)

--- admin/scripts/area_admin_script.js
+++ admin/scripts/area_admin_script.js
@@ -3,31 +3,25 @@ window.onload = async function () {
     const radioBtn = document.querySelectorAll('input[name="tipo-form"]');
     radioBtn.forEach(btn => {
         btn.addEventListener("change", function () {
-            document.getElementById("element1").style.display = "none";
-            document.getElementById("element2").style.display = "none";
-            document.getElementById("element3").style.display = "none";
-            document.getElementById("element4").style.display = "none";
-            document.getElementById("element5").style.display = "none";
+            document.getElementById("edizione-grid").style.display = "none";
+            document.getElementById("stampa-grid").style.display = "none";
             const selected = document.querySelector('input[name="tipo-form"]:checked').value;
             switch (selected) {
                 case '1':
-                    document.getElementById("element1").style.display = "block";
-                    document.getElementById("element2").style.display = "block";
-                    document.getElementById("element3").style.display = "block";
+                    document.getElementById("edizione-grid").style.display="grid";
                     break;
                 case '2':
-                    document.getElementById("element4").style.display = "block";
-                    document.getElementById("element5").style.display = "block";
+                    document.getElementById("stampa-grid").style.display="grid";
                     break;
             }
         });
     });
 
-    //fetch di aggiunta edizioni
+    //fetch di aggiunta edizioni (INSERIMENTO)
     document.getElementById("aggiungi-edizione-form").addEventListener("submit", async (event) => {
         event.preventDefault();
         const form = event.target;
-        const message = document.getElementById("message1");
+        const message = form.querySelector('p');
         message.textContent = "Caricamento in corso...";
         message.style.visibility = "visible";
         //validazione client-side
@@ -67,12 +61,12 @@ window.onload = async function () {
         }
     });
 
-    //fetch di cancellazione edizioni
+    //fetch di cancellazione edizioni (ELIMINAZIONE)
     document.getElementById("cancella-edizione-form").addEventListener("submit", async (event) => {
         event.preventDefault();
         const form = event.target;
         const collocazione = document.getElementById("delete-collocazione-edizione").value;
-        const message = document.getElementById("message2");
+        const message = form.querySelector('p');
         //validazione client-side
         if (!collocazione) {
             message.textContent = "Errore: Collocazione non inserita."
@@ -117,10 +111,10 @@ window.onload = async function () {
         event.preventDefault();
         const cercaForm = event.target;
         const modificaForm = document.getElementById("modifica-edizione-form");
-        const salvaBtn = document.getElementById("salva-btn");
+        const salvaBtn = modificaForm.querySelector('input[type="submit"]');
         const collocazione = document.getElementById("search-collocazione-edizione").value;
-        const message = document.getElementById("message3");
-        const message2 = document.getElementById("message4");
+        const message = cercaForm.querySelector('p');
+        const message2 = modificaForm.querySelector('p');
         message2.style.visibility="hidden";
         message.textContent = "Ricerca in corso...";
         message.style.visibility = "visible";
@@ -155,8 +149,8 @@ window.onload = async function () {
     document.getElementById("modifica-edizione-form").addEventListener("submit", async (event) => {
         event.preventDefault();
         const form = event.target;
-        const message = document.getElementById("message4");
-        const salvaBtn = document.getElementById("salva-btn");
+        const message = form.querySelector('p');
+        const salvaBtn = form.querySelector('input[type="submit"]');
         message.textContent = "Aggiornamento del contenuto in corso...";
         message.style.visibility = "visible";
         const dati = {
@@ -180,21 +174,23 @@ window.onload = async function () {
                 message.textContent = result.message;
                 form.reset();
                 salvaBtn.disabled = true;
-                document.getElementById("message3").style.visibility = "hidden";
-                document.getElementById("cerca-edizione-form").reset();
+                const cercaForm=document.getElementById("cerca-edizione-form");
+                cercaForm.querySelector('p').style.visibility="hidden";
+                cercaForm.reset();
             } else {
-                message.textContent = result.message || "Errore durante l'aggiornamento";
+                message.textContent = result.message || "Errore durante l'aggiornamento.";
             }
         } catch (err) {
             message.textContent = "Errore di rete: impossibile raggiungere il server."
+            console.error("Errore: ", err);
         }
     });
 
-    //fetch di aggiunta stampe
+    //fetch di aggiunta stampe (INSERIMENTO)
     document.getElementById("aggiungi-stampa-form").addEventListener("submit", async (event) => {
         event.preventDefault();
         const form = event.target;
-        const message = document.getElementById("message5");
+        const message = form.querySelector('p');
         message.textContent = "Caricamento in corso...";
         message.style.visibility = "visible";
         //validazione client-side
@@ -234,12 +230,12 @@ window.onload = async function () {
         }
     });
 
-    //fetch di cancellazione stampe
+    //fetch di cancellazione stampe (ELMINAZIONE)
     document.getElementById("cancella-stampa-form").addEventListener("submit", async (event)=>{
         event.preventDefault();
         const form=event.target;
         const collocazione=document.getElementById("delete-collocazione-stampa").value;
-        const message=document.getElementById("message6");
+        const message=form.querySelector('p');
         //validazione client-side
         if(!collocazione){
             message.textContent="Errore: Collocazione non inserita."
@@ -273,6 +269,81 @@ window.onload = async function () {
                 form.reset();
             }else{
                 message.textContent=result.message || "Errore durante la cancellazione.";
+            }
+        }catch(err){
+            message.textContent="Errore di rete: impossibile raggiungere il server."
+        }
+    });
+
+    //fetch di ricerca stampe (MODIFICA)
+    document.getElementById("cerca-stampa-form").addEventListener("submit", async (event)=>{
+        event.preventDefault();
+        const cercaForm=event.target;
+        const modificaForm=document.getElementById("modifica-stampa-form");
+        const salvaBtn=modificaForm.querySelector('input[type="submit"]');
+        const collocazione = document.getElementById("search-collocazione-stampa").value;
+        const message = cercaForm.querySelector('p');
+        const message2 = modificaForm.querySelector('p');
+        message2.style.visibility="hidden";
+        message.textContent = "Ricerca in corso...";
+        message.style.visibility = "visible";
+        salvaBtn.disabled = true;
+        modificaForm.style.display = "none";
+        modificaForm.reset();
+        try{
+            const res= await fetch(`/api/get-stampa/${encodeURIComponent(collocazione)}`);
+            const result=await res.json();
+            if(res.ok && result.success){
+                message.textContent="Contenuto trovato!";
+                //popolamento del form di modifica
+                document.getElementById("update-collocazione-stampa").value=result.dati.collocazione;
+                document.getElementById("update-autore-stampa").value=result.dati.autore;
+                document.getElementById("update-titolo-stampa").value=result.dati.titolo;
+                document.getElementById("update-data_str-stampa").value=result.dati.data_str || "";
+                document.getElementById("update-stampa-stampa").value=result.dati.stampa || "";
+                document.getElementById("update-dimensioni-stampa").value=result.dati.dimensioni || "";
+                salvaBtn.disabled=false;
+                modificaForm.style.display="block";
+            }else{
+                message.textContent=result.message || "Errore durante la ricerca.";
+            }
+        }catch(err){
+            message.textContent="Errore di rete: impossibile raggiungere il server.";
+        }
+    });
+
+    //fetch di modifica stampe (MODIFICA)
+    document.getElementById("modifica-stampa-form").addEventListener("submit", async(event)=>{
+        event.preventDefault();
+        const form=event.target;
+        const message=form.querySelector('p');
+        const salvaBtn=form.querySelector('input[type="submit"]');
+        message.textContent="Aggiornamento del contenuto in corso...";
+        message.style.visibility="visible";
+        const dati={
+            collocazione: document.getElementById("update-collocazione-stampa").value,
+            autore: document.getElementById("update-autore-stampa").value,
+            titolo: document.getElementById("update-titolo-stampa").value,
+            data_str: document.getElementById("update-data_str-stampa").value,
+            stampa: document.getElementById("update-stampa-stampa").value,
+            dimensioni: document.getElementById("update-dimensioni-stampa").value
+        };
+        try{
+            const res=await fetch("/api/update-stampa", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(dati)
+            });
+            const result=await res.json();
+            if(res.ok && result.success){
+                message.textContent=result.message;
+                form.reset();
+                salvaBtn.disabled=true;
+                const cercaForm=document.getElementById("cerca-stampa-form");
+                cercaForm.querySelector('p').style.visibility="hidden";
+                cercaForm.reset();
+            }else{
+                message.textContent=result.message || "Errore durante l'aggiornamento.";
             }
         }catch(err){
             message.textContent="Errore di rete: impossibile raggiungere il server."
