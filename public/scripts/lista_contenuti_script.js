662a26ae9dac09bfe2b7cf8664c3590cc4e1918f
Francesco <francesco.moretti2803@gmail.com>
2026-08-30 20:12:20 +0200

    revisione spaziatura nomi autori e lunghezza titoli su pagine lista

--- public/scripts/lista_contenuti_script.js
+++ public/scripts/lista_contenuti_script.js
@@ -100,11 +100,12 @@ document.addEventListener("DOMContentLoaded", async ()=>{
         lista_da_mostrare.forEach(contenuto=>{
             const tr=document.createElement("tr");
             const tdAutore=document.createElement("td");
+            tdAutore.classList.add("nome-autore");
             tdAutore.textContent=contenuto.autore;
             const tdTitolo=document.createElement("td");
             let titolo=contenuto.titolo;
-            if(titolo.length>75){
-                titolo=titolo.slice(0, 75)+"...";
+            if(titolo.length>100){
+                titolo=titolo.slice(0, 100)+"...";
             }
             //aggiungo link per SEO
             const linkTitolo=document.createElement("a");
